b40f567fedd01fd909b1dd6e6ad8378c142c184f
PirogovaVarvara <spb.vpirogova@gmail.com>
2026-02-24 19:09:58 +0300

    Внесла изменения

--- carRental/carRental/Form1.Designer.cs
+++ carRental/carRental/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace carRental
 {
-    partial class Form1
+    partial class MyForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            CarNumbers = new DataGridViewTextBoxColumn();
+            Brand = new DataGridViewTextBoxColumn();
+            StateNumber = new DataGridViewTextBoxColumn();
+            MileageInKm = new DataGridViewTextBoxColumn();
+            AvgFuelConsumptionPerHourInLiter = new DataGridViewTextBoxColumn();
+            CurrentFuelVolumeInLiters = new DataGridViewTextBoxColumn();
+            CostOfRentPerMinuteInRuble = new DataGridViewTextBoxColumn();
+            FuelReserve = new DataGridViewTextBoxColumn();
+            RentalAmount = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { CarNumbers, Brand, StateNumber, MileageInKm, AvgFuelConsumptionPerHourInLiter, CurrentFuelVolumeInLiters, CostOfRentPerMinuteInRuble, FuelReserve, RentalAmount });
+            dataGridView1.Location = new Point(39, 156);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(943, 145);
+            dataGridView1.TabIndex = 0;
+            // 
+            // CarNumbers
+            // 
+            CarNumbers.HeaderText = "№ машины";
+            CarNumbers.Name = "CarNumbers";
+            // 
+            // Brand
+            // 
+            Brand.DataPropertyName = "Brand";
+            Brand.HeaderText = "Марка";
+            Brand.Name = "Brand";
+            // 
+            // StateNumber
+            // 
+            StateNumber.DataPropertyName = "StateNumber";
+            StateNumber.HeaderText = "Гос номер";
+            StateNumber.Name = "StateNumber";
+            // 
+            // MileageInKm
+            // 
+            MileageInKm.DataPropertyName = "MileageInKm";
+            MileageInKm.HeaderText = "Пробег";
+            MileageInKm.Name = "MileageInKm";
+            // 
+            // AvgFuelConsumptionPerHourInLiter
+            // 
+            AvgFuelConsumptionPerHourInLiter.DataPropertyName = "AvgFuelConsumptionPerHourInLiter";
+            AvgFuelConsumptionPerHourInLiter.HeaderText = "Средний расход топлива за час";
+            AvgFuelConsumptionPerHourInLiter.Name = "AvgFuelConsumptionPerHourInLiter";
+            // 
+            // CurrentFuelVolumeInLiters
+            // 
+            CurrentFuelVolumeInLiters.DataPropertyName = "CurrentFuelVolumeInLiters";
+            CurrentFuelVolumeInLiters.HeaderText = "Текущий объем топлива";
+            CurrentFuelVolumeInLiters.Name = "CurrentFuelVolumeInLiters";
+            // 
+            // CostOfRentPerMinuteInRuble
+            // 
+            CostOfRentPerMinuteInRuble.DataPropertyName = "CostOfRentPerMinuteInRuble";
+            CostOfRentPerMinuteInRuble.HeaderText = "Стоимость аренды";
+            CostOfRentPerMinuteInRuble.Name = "CostOfRentPerMinuteInRuble";
+            // 
+            // FuelReserve
+            // 
+            FuelReserve.DataPropertyName = "FuelReserve";
+            FuelReserve.HeaderText = "Запас хода топлива";
+            FuelReserve.Name = "FuelReserve";
+            // 
+            // RentalAmount
+            // 
+            RentalAmount.DataPropertyName = "RentalAmount";
+            RentalAmount.HeaderText = "Сумма аренды";
+            RentalAmount.Name = "RentalAmount";
+            // 
+            // MyForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(1154, 450);
+            Controls.Add(dataGridView1);
+            Name = "MyForm";
+            Text = "MyForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn CarNumbers;
+        private DataGridViewTextBoxColumn Brand;
+        private DataGridViewTextBoxColumn StateNumber;
+        private DataGridViewTextBoxColumn MileageInKm;
+        private DataGridViewTextBoxColumn AvgFuelConsumptionPerHourInLiter;
+        private DataGridViewTextBoxColumn CurrentFuelVolumeInLiters;
+        private DataGridViewTextBoxColumn CostOfRentPerMinuteInRuble;
+        private DataGridViewTextBoxColumn FuelReserve;
+        private DataGridViewTextBoxColumn RentalAmount;
     }
 }
